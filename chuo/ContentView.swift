ae938f3018452a2340201a8002e53a5a6ea25914
purple1109 <takanorim119@gmail.com>
2024-11-19 17:21:18 +0900

    Initial Commit

---
+++ chuo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chuo
+//
+//  Created by 村崎　尊紀  on 2024/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chuo/chuoApp.swift
@@ -0,0 +1,17 @@
+//
+//  chuoApp.swift
+//  chuo
+//
+//  Created by 村崎　尊紀  on 2024/11/19.
+//
+
+import SwiftUI
+
+@main
+struct chuoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
